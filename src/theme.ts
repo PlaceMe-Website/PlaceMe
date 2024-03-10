19658da23a3d53943cc6fca4892ac62d3364de23
hpuranik <harshapuranik1704@gmail.com>
2024-03-10 16:28:16 -0400

    Adding sample data file and updating search results

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,13 @@
 'use client';
 import { Roboto } from 'next/font/google';
-import { createTheme } from '@mui/material/styles';
+import {  createTheme } from '@mui/material/styles';
+// }
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Unstable_Grid2';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+
 
 const roboto = Roboto({
   weight: ['300', '400', '500', '700'],
@@ -26,6 +33,18 @@ const theme = createTheme({
       },
     },
   },
+  
 });
+
+
+export const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: 'left',
+  color: theme.palette.text.secondary,
+}));
+
+
 
 export default theme;
